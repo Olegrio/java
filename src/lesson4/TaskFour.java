9959f4d35d47f75dd07c265f1dcf9dbde6f1b885
Oleg Fomichev <fomichevolegv@gmail.com>
2021-03-24 19:09:02 +0300

    lesson4 -> TaskFour правка

--- src/lesson4/TaskFour.java
+++ src/lesson4/TaskFour.java
@@ -7,7 +7,12 @@ public class TaskFour {
         // 4. Создать массив из чётных чисел [2, 4, 6, 8, 10, 12 ... 18, 20]
         // и вывести элементы массива в консоль в обратном порядке (20 18 16 ... 4 2)
 
-        int[] array = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20} ;
+        int[] array = new int[11];
+        for (int i = 0, value = 0; i < array.length; i++, value +=2 ) {
+            array[i] = value;
+        }
+
+        //int[] array = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20} ;
 
         int[] newArray = array.clone();
         for (int i = 0; i < newArray.length; i++) {
